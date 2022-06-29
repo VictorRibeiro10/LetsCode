2c7c36487b70578bf5cb9da7e7477945e5045909
victor <victorsilvaribeiro@hotmail.com>
2022-06-28 21:31:48 -0300

    Projeto Let's Code - Comentario

--- src/main/java/victor/sistemaCriticaFilmes/letsCode/entities/comentario.java
+++ src/main/java/victor/sistemaCriticaFilmes/letsCode/entities/comentario.java
@@ -1,5 +1,20 @@
 package victor.sistemaCriticaFilmes.letsCode.entities;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Component
+@Table(name = "tb_comentario")
+@Data
 public class comentario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idComentario;
+    private Boolean gostou;
+    private String resposta;
+    private Long idUsuario;
     //gostou, resposta, excluir, repetido, idUsuario, Idfilme, descriçaoComentario
 }
